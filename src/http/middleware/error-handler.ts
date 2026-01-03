0eb0a47057ebf654818fc5167af088a3813937f9
Ekene <83151271+Yeahx1000@users.noreply.github.com>
2026-01-03 14:04:00 -0500

    error handler + logger additions

--- src/http/middleware/error-handler.ts
+++ src/http/middleware/error-handler.ts
@@ -5,5 +5,10 @@ export const errorHandler = (
   req: FastifyRequest,
   res: FastifyReply
 ) => {
+  console.error(err);
+  res.status(err.statusCode || 500).send({
+    message: err.message || 'Internal Server Error',
+    code: err.code || undefined,
+  });
 };
 
--- src/http/middleware/logger.ts
+++ src/http/middleware/logger.ts
@@ -1,5 +1,8 @@
 import { FastifyRequest, FastifyReply } from 'fastify';
 
 export const logger = async (req: FastifyRequest, res: FastifyReply) => {
+    const start = Date.now();
+    const duration = Date.now() - start;
+    req.log.info(`${req.method} ${req.url} ${res.statusCode} ${duration}ms`);
 };
 
